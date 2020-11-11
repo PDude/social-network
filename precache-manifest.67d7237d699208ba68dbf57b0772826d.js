dbfed6bd0bc382534c25045a4e6534a0bff6d86e
Pavlo Dudych <dudich.pasha@gmail.com>
2020-11-11 06:49:23 +0200

    Updates

---
+++ precache-manifest.67d7237d699208ba68dbf57b0772826d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2610360f99d439e59126da9f4cc135",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "87adda2efa67788bb078",
+    "url": "/social-network/static/css/3.b0a8fae1.chunk.css"
+  },
+  {
+    "revision": "2df9656f7636fb46fc74",
+    "url": "/social-network/static/css/4.745c16de.chunk.css"
+  },
+  {
+    "revision": "bb2b477325625b0e66fb",
+    "url": "/social-network/static/css/5.efcdf81b.chunk.css"
+  },
+  {
+    "revision": "c594debaa9576fac48b6",
+    "url": "/social-network/static/css/main.0a4f67d3.chunk.css"
+  },
+  {
+    "revision": "b04308f2764906adae72",
+    "url": "/social-network/static/js/2.5e8bee80.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/social-network/static/js/2.5e8bee80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87adda2efa67788bb078",
+    "url": "/social-network/static/js/3.3ca4b039.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/social-network/static/js/3.3ca4b039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df9656f7636fb46fc74",
+    "url": "/social-network/static/js/4.0dbdff0d.chunk.js"
+  },
+  {
+    "revision": "bb2b477325625b0e66fb",
+    "url": "/social-network/static/js/5.c07b4892.chunk.js"
+  },
+  {
+    "revision": "204f3e17705171c8e9d5",
+    "url": "/social-network/static/js/6.7100f524.chunk.js"
+  },
+  {
+    "revision": "c594debaa9576fac48b6",
+    "url": "/social-network/static/js/main.2b3c633d.chunk.js"
+  },
+  {
+    "revision": "5208c0cf139331cfa141",
+    "url": "/social-network/static/js/runtime-main.b9e15029.js"
+  },
+  {
+    "revision": "7618a1c68eb09cd8459a92a07f8bcdbd",
+    "url": "/social-network/static/media/cover_img.7618a1c6.jfif"
+  },
+  {
+    "revision": "0f8db70ff4586474ed44d8506c27d45a",
+    "url": "/social-network/static/media/loader.0f8db70f.svg"
+  },
+  {
+    "revision": "9b2ad50fbf44a957178db3a29266466a",
+    "url": "/social-network/static/media/profile_img_plug.9b2ad50f.jpg"
+  }
+]);
